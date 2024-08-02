b7fc432dc2345c377a1863da7e6073c649dab1dd
ch405maki <markmanuel0317@gmail.com>
2024-08-02 10:19:15 +0800

    working on footer

---
+++ resources/js/Pages/Main/Partials/RelatedWeb.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- Start Related Websites -->
+    <div class="flex flex-wrap justify-center items-start space-x-8 my-8">
+        <div class="flex flex-col items-center w-24">
+        <a href="https://www.arellano.edu.ph/" class="text-center">
+            <img class="w-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 transform hover:scale-105" src="/images/auLogo.png" alt="Logo">
+            <span class="mt-2 block w-full text-center text-slate-700">Arellano University</span>
+        </a>
+        </div>
+        <div class="flex flex-col items-center w-24">
+        <a href="https://clear.arellanolaw.org/" class="text-center">
+            <img class="w-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 transform hover:scale-105" src="/images/alflogo.png" alt="Logo">
+            <span class="mt-2 block w-full text-center text-slate-700">MCLE</span>
+        </a>
+        </div>
+        <div class="flex flex-col items-center w-24">
+        <a href="https://lawphil.net/" class="text-center">
+            <img class="w-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 transform hover:scale-105" src="/images/lawphilLogo.png" alt="Logo">
+            <span class="mt-2 block w-full text-center text-slate-700">LawPhil</span>
+        </a>
+        </div>
+        <div class="flex flex-col items-center w-24">
+        <a href="https://ichief.arellanolaw.org/" class="text-center">
+            <img class="w-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 transform hover:scale-105" src="/images/alflogo.png" alt="Logo">
+            <span class="mt-2 block w-full text-center text-slate-700">IChief</span>
+        </a>
+        </div>
+        <div class="flex flex-col items-center w-24">
+        <a href="https://arellanolaw.org/" class="text-center">
+            <img class="w-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 transform hover:scale-105" src="/images/alflogo.png" alt="Logo">
+            <span class="mt-2 block w-full text-center text-slate-700">Arellano Law Foundation</span>
+        </a>
+        </div>
+    </div>
+    <!-- End Related Websites -->
+</template>
