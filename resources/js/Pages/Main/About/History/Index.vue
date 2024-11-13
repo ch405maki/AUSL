79bc671ffaeb6f6403e6e82f4873b8dd42b17fe3
ch405maki <markmanuel0317@gmail.com>
2024-11-13 15:24:21 +0800

    udjust cookie pop up

--- resources/js/Pages/Main/About/History/Index.vue
+++ resources/js/Pages/Main/About/History/Index.vue
@@ -56,8 +56,8 @@
           </section>
       </div>
     </div>
-    <MissionVision />
-    <!-- <MissionVisionOld /> -->
+    <!-- <MissionVision /> -->
+    <MissionVisionOld />
     <Hymn />
   </main>
 </MainLayout>
