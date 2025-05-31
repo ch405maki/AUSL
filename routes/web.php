95484fcc9568c4c6972ec4035fa59e0f5396910b
ch405maki <markmanuel0317@gmail.com>
2025-05-31 14:08:59 +0800

    update link

--- routes/web.php
+++ routes/web.php
@@ -235,8 +235,11 @@ Route::get('/privacy_policy', function () {
 
 // Relevant Links
 
+    // Route::get('/manual', function () {
+    //     return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSdKYmxSPSOLMZ_Fih8L9ojrDT-KoSbJlb1qjZ4C5RlpD4dGrA/viewform');
+    // });
     Route::get('/manual', function () {
-        return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSdKYmxSPSOLMZ_Fih8L9ojrDT-KoSbJlb1qjZ4C5RlpD4dGrA/viewform');
+        return redirect()->away('https://docs.google.com/forms/d/1xcswBgEopj_hGhdlcqN-Tvu6ZFV7Wfuxu4qhFiJJDm0/preview');
     });
     Route::get('/medical', function () {
         return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLScZXZAGt2BnVhXl35xgSxLnj8CLWlcL0FImY2jz90PzJofpPw/viewform');
@@ -245,7 +248,7 @@ Route::get('/privacy_policy', function () {
         return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSeOl0S9Pwy1Q-e1RRJYg1CA85NmY4nnF_b-wVQSxyFmCwU1qA/viewform');
     });
     Route::get('/adding', function () {
-        return redirect()->away('https://forms.gle/3FpSCS2ZdoRW8sXS8');
+        return redirect()->away('https://docs.google.com/forms/d/1xcswBgEopj_hGhdlcqN-Tvu6ZFV7Wfuxu4qhFiJJDm0/preview');
     });
     Route::get('/beadles', function () {
         return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSegRiW6hkI3QLGhNOB9DxAhgxErDzovW8AaJx3_a0gZMtQXOQ/viewform');
